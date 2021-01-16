52b3bf0fb92233b00fba61c056bcfd16cb0071dc
Nathaniel Palmer <npalmer@follett.com>
2021-01-16 15:01:30 -0500

    add Scully

---
+++ scully.scully-blog-test.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "scully-blog-test",
+  outDir: './dist/static',
+  routes: {
+  }
+};
