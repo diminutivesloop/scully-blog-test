c75665cef593a84aa0faabe40ab8d56b5aa0bb6f
Nathaniel Palmer <npalmer@follett.com>
2021-01-22 15:26:32 -0500

    fix mdReplaceRoute custom plugin config

--- scully.scully-blog-test.config.ts
+++ scully.scully-blog-test.config.ts
@@ -1,11 +1,13 @@
 import { ScullyConfig } from '@scullyio/scully';
+import { mdReplaceRoute } from "./scully/plugins/plugin";
+
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "scully-blog-test",
   outDir: './dist/static',
   routes: {
     "/blog": {
-      type: "mdReplaceRoute",
+      type: mdReplaceRoute,
       file: "./blog/home.md",
     },
     '/blog/:slug': {
